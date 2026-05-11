90588ad9eead326f9bb44eb2491b56802c62a0db
audiopwn <189563860+audiopwn@users.noreply.github.com>
2026-05-11 10:42:13 +0900

    use peqdb/autoeq-c/js for autoeq

--- assets/js/tool_components/eqPanel.js
+++ assets/js/tool_components/eqPanel.js
@@ -63,6 +63,7 @@ function addExtra() {
         if (oldPhoneObj) {
             oldPhoneObj.active && removePhone(oldPhoneObj);
             phoneObj.id = oldPhoneObj.id;
+            phoneObj.offset = oldPhoneObj.offset;
             phoneObjs[phoneObjs.indexOf(oldPhoneObj)] = phoneObj;
             allPhones[allPhones.indexOf(oldPhoneObj)] = phoneObj;
         } else {
@@ -231,13 +232,18 @@ function addExtra() {
             phoneObj.rawChannels.map(c => c ? Equalizer.apply(c, filters) : null));
         phoneObj.eq = phoneObjEQ;
         phoneObjEQ.eqParent = phoneObj;
+        if (typeof pendingEqOffset === "number" && isFinite(pendingEqOffset)) {
+            phoneObjEQ.offset = pendingEqOffset;
+            pendingEqOffset = null;
+        }
         updatePreampDisplay();
         showPhone(phoneObjEQ, false);
         activeElem.focus();
     };
+    let pendingEqOffset = null;
     let applyEQ = () => {
         clearTimeout(applyEQHandle);
-        applyEQHandle = setTimeout(applyEQExec, 100);
+        applyEQHandle = setTimeout(applyEQExec, 1);
         updateFilters(elemToFilters());
         document.dispatchEvent(new CustomEvent('UpdateExtensionFilters', { detail: { filters: elemToFilters() } }));
     };
@@ -403,9 +409,10 @@ function addExtra() {
     document.querySelector("div.extra-eq button.readme").addEventListener("click", () => {
         alert("1. If you want to AutoEQ model A to B, display A B and remove target\n" +
             "2. Add/Remove bands before AutoEQ may give you a better result\n" +
-            "3. Curve of PK filter close to 20K is implementation dependent, avoid such filter if you're not sure how your DSP software works\n" +
+            "3. The behavior of filters close to 20K is implementation dependent, avoid such filter if you're not sure how your DSP software works\n" +
             "4. EQ treble require resonant peak matching and fine tune by ear, keep treble untouched if you're not sure how to do that\n" +
-            "5. Tone generator is useful to find actual location of peaks and dips, notice the web version may not work on some platform\n");
+            "5. Tone generator is useful to find actual location of peaks and dips, notice the web version may not work on some platform\n\n" +
+            "JS port of AutoEQ algorithm provided by PEQdB (https://github.com/peqdb/autoeq-c)\n");
     });
     // AutoEQ
     let autoEQFromInput = document.querySelector("div.extra-eq input[name='autoeq-from']");
@@ -439,7 +446,7 @@ function addExtra() {
         }
         let autoEQOverlay = document.querySelector(".extra-eq-overlay");
         autoEQOverlay.style.display = "block";
-        setTimeout(() => {
+        setTimeout(async () => {
             let autoEQFrom = Math.min(Math.max(parseInt(autoEQFromInput.value) || 0, 20), 20000);
             let autoEQTo = Math.min(Math.max(parseInt(autoEQToInput.value) || 0, autoEQFrom), 20000);
             Equalizer.config.AutoEQRange = [autoEQFrom, autoEQTo];
@@ -453,11 +460,16 @@ function addExtra() {
                 .map(ch => ch.map(([f, v]) => [f, v + phoneObj.norm])));
             let phoneCH = (phoneCHs.length > 1) ? avgCurves(phoneCHs) : phoneCHs[0];
             let targetCH = targetObj.rawChannels.filter(c => c)[0].map(([f, v]) => [f, v + targetObj.norm]);
-            let filters = Equalizer.autoeq(phoneCH, targetCH, eqBands);
+
+            let [filters, offset] = await Equalizer.autoeq(
+                phoneCH, targetCH, eqBands, typeof autoEqMode === 'undefined' ? 'IE' : autoEqMode);
+
             filtersToElem(filters);
+            pendingEqOffset = offset;
             applyEQ();
+
             autoEQOverlay.style.display = "none";
-        }, 100);
+        }, 1);
     });
 
     //* Pre amp Calc display *//
